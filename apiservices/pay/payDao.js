4d0ebb685f4378c31f0483d708a4ef8b49d75983
nicoContigliani <nico.contigliani@gmail.com>
2022-07-10 21:54:33 -0300

    services create

--- apiservices/pay/payDao.js
+++ apiservices/pay/payDao.js
@@ -4,7 +4,7 @@ const uuid = require('uuid').v4
 const getPayAll = async () => {
 
     try {
-        const response = await pool.query('select * from public.payments ;');
+        const response = await pool.query('SELECT * FROM payments INNER JOIN  payments_exchange pe ON payments.id = pe.id_exchange ;');
         post = response.rows
         return post
     } catch (error) {
@@ -27,27 +27,33 @@ const getIdPay = async (id_user) => {
 const savePay = async (resource) => {
     const id = uuid()
     const { valor, fecha } = resource.resultado
-    const { description, billed_hours, billed_at, billing_currency, billed_amount, needs_exchange, exchange_currency, created_at, updated_at } = resource.resource;
+    const { object, description, billed_at, billed_hours, amount, currency, created_at, updated_at } = resource.resource;
     console.log(resource, "esto me llega a dao pay")
 
     try {
         if (description === "Pago" || needs_exchange === true) {
             console.log("si entro")
             const id_exchange = id
+            const valors = parseInt(valor)
             const amount = parseInt(billed_at) * parseInt(valor)
-            //console.log(amount ,"esto es amount  ")
+            const exchangeexchange_rate = valors
+            const exchangecurrency = currency
+            const original_amount = billed_hours
+            const exchange_rate = valors
+            const response = await pool.query(`INSERT INTO public.payments (id, "object", description, billed_at, billed_hours, amount, currency, created_at, updated_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)`, [id, object, description, billed_at, billed_hours, amount, currency, created_at, updated_at]);
 
-            const response = await pool.query(`INSERT INTO public.payments (id, description, billed_hours, billet_at, billing_currency, billed_amount, needs_exchange, exchange_currency, created_at, updated_at,amount) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)`, [id, object, description, billed_at, billed_hours, billing_currency, currency, exchangeoriginal_amount, exchangecurrency, exchangeexchange_rate, created_at = new Date(), updated_at, amount]);
-            const res = await pool.query(` INSERT INTO public.payments_exchange (id_exchange, original_amount, currenci, exchange_rate) VALUES ($1,$2,$3,$4)`, [id_exchange, original_amount, currenci, exchange_rate]);
+            const sec = await pool.query(` INSERT INTO public.payments_exchange (id_exchange, original_amount, currency, exchange_rate) VALUES ($1,$2,$3,$4)`, [id_exchange, original_amount, currency, exchange_rate]);
+
 
         } else {
             console.log("no entro")
-              const response = await pool.query(`INSERT INTO public.payments (id, description, billed_hours, billet_at, billing_currency, billed_amount, needs_exchange, exchange_currency, created_at, updated_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11)`, [id, object, description, billed_at, billed_hours, amount, currency, exchangeoriginal_amount, exchangecurrency, exchangeexchange_rate, created_at = new Date(), updated_at,amount=billed_amount]);
+            const response = await pool.query(`INSERT INTO public.payments (id, "object", description, billed_at, billed_hours, amount, currency, created_at, updated_at) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9)`, [id, object, description, billed_at, billed_hours, amount, currency, created_at, updated_at]);
+            const ress = await pool.query(` INSERT INTO public.payments_exchange (id_exchange, exchangeoriginal_amount, exchangecurrency, exchangeexchange_rate) VALUES ($1,$2,$3,$4)`, [id_exchange = id, exchangeoriginal_amount = billed_hours, exchangecurrency = currency, exchangeexchange_rate = valor]);
+
         }
     } catch (error) {
         console.log(error)
     }
-
 
 
     // try {
